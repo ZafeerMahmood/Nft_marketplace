a63dfe584c10bb2872bc00b17bc8e4a32a311621
Zafeer Mahmood <zafeer7460@gmail.com>
2023-07-18 15:48:10 +0500

    fixed bug with google login

--- pages/GoogleAuth.js
+++ pages/GoogleAuth.js
@@ -1,18 +1,15 @@
 
 import React from "react";
-
-import { useState } from 'react';
-import { useRouter } from "next/router";
 import { Suspense } from "react";
-const Google_Auth = React.lazy(()=> (import("../components/GoogleAuth/GoogleAuthLayout")))
+const Google_Auth = React.lazy(() => (import("../components/GoogleAuth/GoogleAuthLayout")))
 import Discover_Loading from "../components/Dynamic_Pages_Layout/Discover_Loading";
 function GoogleAuth() {
   return (
 
     <div >
       <Suspense fallback={<div>{Discover_Loading()}</div>}>
-<Google_Auth/>
-</Suspense>
+        <Google_Auth />
+      </Suspense>
     </div>
 
   );
--- pages/MetamaskAuth.js
+++ pages/MetamaskAuth.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Metamask_Auth = React.lazy(()=>(import ("../components/MetamaskAuth/MetamaskAuthLayout")))
+const Metamask_Auth = React.lazy(() => (import("../components/MetamaskAuth/MetamaskAuthLayout")))
 import Discover_Loading from "../components/Dynamic_Pages_Layout/Discover_Loading";
 import { Suspense } from "react";
 function MetamskAuth() {
   return (
 
     <div >
-<Suspense fallback={<div>{Discover_Loading()}</div>}>
-      <Metamask_Auth />
+      <Suspense fallback={<div>{Discover_Loading()}</div>}>
+        <Metamask_Auth />
       </Suspense>
     </div>
 
